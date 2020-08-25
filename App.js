1e922d8d0e383b66f226e4fe658845fb0f8616a1
dank0r <dan296dan@gmail.com>
2020-08-26 00:16:46 +0300

    add bottom navigation

--- App.js
+++ App.js
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 import styled from 'styled-components/native';
 import { loadAsync } from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feed from './src/Feed';
+import Schedule from './src/Schedule';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
@@ -23,6 +29,45 @@ export default function App() {
   }
 
   return (
-    <Feed />
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            switch(route.name) {
+              case 'Feed':
+                iconName = 'ios-home';
+                break;
+              case 'Schedule':
+                iconName = focused ? 'ios-list-box' : 'ios-list';
+                break;
+              case 'Profile':
+                iconName = 'ios-person';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          tabStyle: {
+            justifyContent: 'center',
+          },
+          style: {
+            backgroundColor: '#1E2631',
+            borderTopWidth: 0,
+            borderTopColor: '#1E2631',
+            outline: 'none',
+          },
+          showLabel: false,
+          activeTintColor: '#FF5045'
+        }}
+      >
+        <Tab.Screen name="Feed" component={Feed} options={{title: 'Лента'}} />
+        <Tab.Screen name="Schedule" component={Schedule} options={{title: 'Расписание'}} />
+        <Tab.Screen name="Profile" component={Schedule} options={{title: 'Профиль'}} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ src/Schedule.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import styled from 'styled-components/native';
+
+export default function Schedule() {
+  return (
+    <View/>
+  );
+}
